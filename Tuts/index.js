a7cdc5dd20193ec2d73a73fd9a62f7ff39cf979f
Rajat Kumar 2 <rajkumar24@publicisgroupe.net>
2019-04-11 13:35:02 +0530

    Tutorials promises async

--- Tuts/index.js
+++ Tuts/index.js
@@ -33,3 +33,58 @@ const anotherToArray = (...args) => {
 // Rest operator looks just like spread operator
 // Spread operator is used to extract array items or object's propertie and methods
 // While rest operator is used to bundle arguments into array
+
+
+
+// ASYNC CODE
+const fetchData = callback => {
+    setTimeout(()=>{
+        callback('DONE!!');
+    },1500);
+};
+
+// Nested CB
+const getData = () => {
+    setTimeout(()=>{
+        console.log('ASYNC CALL DONE!!')
+        fetchData(text => console.log(text));
+    }, 2000);
+};
+
+// Another solution - PROMISES
+const fetchData1 = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve('DONE!!');
+        },2000);
+    });
+    return promise;
+};
+
+const getData1 = () => {
+    setTimeout(() => {
+        console.log('ASYNC Promises');
+        fetchData1().then(data => console.log(data));
+    }, 2400);
+};
+
+// Its better because we don't have to use nested promises again and again like in callbacks
+// Promises can be chained, one promise can return another promise, so they all can be chained
+
+// Chaining example
+const getData2 = () => {
+    setTimeout(() => {
+        console.log('ASYNC Promises');
+        fetchData1()
+            .then(data => {
+                console.log('PROMISE 1');
+                console.log(data);
+                return fetchData1();
+            }).then(data2 => {
+                console.log('PROMISE 2');
+                console.log(data2);
+            })
+    }, 2400);
+};
+
+getData2();
