67d85047475b9e0aac51994a61093035e3971dc6
Rajat Kumar 2 <rajkumar24@publicisgroupe.net>
2019-04-16 17:21:59 +0530

    Using anonymous function

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const http = require('http');
 // 1. request of type "IncomingMessage"
 // 2. response of type "ServerResponse"
 // Node.js automatically gives us the incoming request as an object and response which we can use to send a response to whoever sent that request
-function reqListener(req, res) {
+
+
+// With anonymous function
+http.createServer(function (req, res) {
     console.log(req);
-};
-
-http.createServer(reqListener);
-
+});
 
