b4de7b0ef48ca5bdc4d8ff8cb11ab0a8882b9e80
Rajat Kumar 2 <rajkumar24@publicisgroupe.net>
2019-04-16 17:21:23 +0530

    Adding HTTP module, create a server and adding request listener

---
+++ app.js
@@ -0,0 +1,18 @@
+// Root file
+// Server file of node app
+// Server will run from here
+
+// Const as we will use it but will never redefined it
+const http = require('http');
+
+// Request listener receives 2 arguments
+// 1. request of type "IncomingMessage"
+// 2. response of type "ServerResponse"
+// Node.js automatically gives us the incoming request as an object and response which we can use to send a response to whoever sent that request
+function reqListener(req, res) {
+    console.log(req);
+};
+
+http.createServer(reqListener);
+
+
