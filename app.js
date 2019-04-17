99a45e3636747edc922021c3cb2956dcdfdcad7b
Rajat Kumar 2 <rajkumar24@publicisgroupe.net>
2019-04-17 10:30:57 +0530

    Listen method of server

--- app.js
+++ app.js
@@ -11,8 +11,14 @@ const http = require('http');
 // Node.js automatically gives us the incoming request as an object and response which we can use to send a response to whoever sent that request
 
 
-// With anonymous function
-http.createServer((req, res) => {
+// http.createServer return a Server
+const server = http.createServer((req, res) => {
     console.log(req);
 });
+
+// Server has a bunch of methods to call, one of them is listen
+// Listen starts a process when nodejs not immediately exit our script, but nodejs keeps it running to listen to incoming requests
+server.listen();
+
+ 
 
