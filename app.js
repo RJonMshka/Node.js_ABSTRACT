db6c3cd1a4e13ab54b15bc16233677ee181dd793
Rajat Kumar 2 <rajkumar24@publicisgroupe.net>
2019-04-17 10:46:47 +0530

    Listening on a port & understanding server listen

--- app.js
+++ app.js
@@ -18,7 +18,13 @@ const server = http.createServer((req, res) => {
 
 // Server has a bunch of methods to call, one of them is listen
 // Listen starts a process when nodejs not immediately exit our script, but nodejs keeps it running to listen to incoming requests
-server.listen();
+// Listen takes optional arguments
+// 1. port on which server will listen (On prod, we will typically not fill this out and it will take a default of 80), on dev, will give it a "number"
+// 2. Hostname: string, by default it will be the name of machine it is running on , on local machine it is "localhost"
+// Thousand ports are pretty safe
+// It will have ongoing looping process that keeps the app running
+// You can hit localhost:3000 on browser to check the request logged on to console
+server.listen(3000);
 
  
 
