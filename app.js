6d157d5dc63743a0a9d1098e60ffb276ba5a6eba
Rajat Kumar 2 <rajkumar24@publicisgroupe.net>
2019-04-18 10:47:14 +0530

    Redirecting

--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 // Server file of node app
 // Server will run from here
 const http = require('http');
+// FS allows us to work with the file system
+const fs = require('fs');
 
 // http.createServer return a Server
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     if(url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
@@ -14,6 +17,18 @@ const server = http.createServer((req, res) => {
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>');
         res.write('</html>');
         // Return statement is added to return from anonymous function and do not run the below code
+        return res.end();
+    }
+    if(url === '/message' && method === 'POST') {
+        // writeFileSync takes 1st arg as Path to the file, 2nd arg as data to be stored in that file
+        fs.writeFileSync('message.txt','DUMMY');
+        // res.writeHead allows us to write some meta info in one go. 1st arg is statusCode and other is an object with key value pair signifying headers you want to send in response object
+        // res.writeHead(302, {});
+
+        // OR you can break it down like this
+        res.statusCode = 302;
+        // 302 is for REDIRECTION
+        res.setHeader('Location', '/');
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
