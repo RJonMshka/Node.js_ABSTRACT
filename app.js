e998d14ed14fcbe78178a8cb309c979351363278
Rajat Kumar 2 <rajat1996kumar@gmail.com>
2019-05-06 12:13:27 +0530

    EXPRESS starting

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
 // Server file of node app
 // Server will run from here
 const http = require('http');
-const routes = require('./routes');
 
-// http.createServer return a Server
-const server = http.createServer(routes.handler);
+const express = require('express');
+
+// Now you create an express application and store it in a variable by calling express as a function
+const app = express();
+
+// app also happens to be a valid request handler
+const server = http.createServer(app);
 
 server.listen(3000);
 
