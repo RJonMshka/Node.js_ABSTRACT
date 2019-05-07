6709a35cf643f6053fe146b2b65288928b911d96
Rajat Kumar 2 <rajat1996kumar@gmail.com>
2019-05-07 13:18:22 +0530

    Body Parser for forms

--- app.js
+++ app.js
@@ -4,33 +4,26 @@ const express = require('express');
 
 // Now you create an express application and store it in a variable by calling express as a function
 const app = express();
+const bodyParser = require('body-parser');
 
-// To use middleware, we use method USE of app
-// It accepts array of request handlers
-// Can pass a function here, which will execute for every request
+// Body parser for forms
+app.use(bodyParser.urlencoded({extended: false}));
 
-// It will receive 3 ARGS
-// REQ -> req with extra features
-// RES -> res with extra features
-// NEXT -> a function, passed to this handler by express js
-// It has to be executed to allow req to move on or transfer to the next middleware
 app.use( (req, res, next) => {
     console.log('In the middleware Now!!!');
-    next(); // Allows request tocontinue to the next middleware in line
+    next(); 
 });
 
 app.use('/add', (req, res, next) => {
-    res.send('<h1>Add product Page</h1>');
+    res.send('<form action="/users" method="POST"><input type="text" name="username"/><button type="submit">ADD ME</button></form>');
 });
 
-// Path does n't mean full path but the start of the path of our request
-// So '/' path matches all requests, as all requests will have / at beginning
-app.use('/', (req, res, next) => {
-    console.log('In another middleware Now!!!');
-    // Can set header manuaaly as well and override the default express header setting
+app.use('/users', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
 
-    // Send  allows us to send a response
-    // Automatically sets content-type HTML 
+app.use('/', (req, res, next) => {
     res.send('<h1>Hello From Express!!</h1>');
 });
 
