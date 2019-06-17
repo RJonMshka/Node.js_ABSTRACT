c7f6e04bee214307867c163dd306ef30d8c70bc7
Rajat Kumar 2 <rajat1996kumar@gmail.com>
2019-06-17 17:44:25 +0530

    Creating Models and Contoller

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 // Now you create an express application and store it in a variable by calling express as a function
 const app = express();
+const errorController = require('./controllers/error');
 
 // We can set express configurations by using app.set (docs)
 app.set('view engine', 'ejs');
@@ -15,7 +16,7 @@ app.set('views', 'views');
 
 const bodyParser = require('body-parser');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const  shopRouter = require('./routes/shop');
 
 // Body parser for forms
@@ -28,15 +29,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // USe admin Router
 // Filter admin requests
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 
 // Use Shop Router
 app.use(shopRouter); 
 
-app.use((req, res, next) => {
-    // Can chain methods like below
-    res.status(404).render('404', {pageHeading: "Page Not Found !!!!!!!!", pageTitle: "Page Not Found"})
-});
+app.use(errorController.get404);
 
 // Starting & Listening to the server
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,29 +1,10 @@
-const path = require('path');
-
 const express = require('express');
 
-const rootDir = require('../utils/path');
-
-const adminData = require('./admin');
-
+const productController = require('../controllers/products');
 // Express Router
 // Its like a mini-express app tied to other express app (plugable)
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    // We use res.render
-    // No need to do .pug as we already specified in app that we are using pug for templating
-    // Also no need to specify path as we already specified it in app.js
-    const products = adminData.products;
-    res.render('shop',
-        {
-            prods: products, pageTitle: "Shop",
-            path: "/",
-            hasProducts: products.length > 0,
-            activeShop: true,
-            productCSS: true
-        }
-    );
-});
+router.get('/', productController.getProducts);
 
 module.exports = router;
